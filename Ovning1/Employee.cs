da90efb2f3b3dda614950e918399496f7616fab1
Fatma Alhouthi <fa222nx@student.lnu.se>
2020-08-25 15:39:29 +0200

    add more methods

--- Ovning1/Employee.cs
+++ Ovning1/Employee.cs
@@ -7,14 +7,38 @@ namespace Ovning1
     class Employee
     {
 
-        public void EmployeeNameSalary()
+      static List<string> EmployeeNameList = new List<string>();
+       static List<float> EmployeeSarayList = new List<float>();
+       
+
+        public void EmployeeName()
         {
             Console.WriteLine("Enter employee name please:");
             string employeeName = Console.ReadLine();
-            Console.WriteLine("Enter employee salary please:");
-            float employeeSalary = int.Parse(Console.ReadLine());
-            Console.WriteLine("name: " + employeeName + "     salary: " + employeeSalary + "kr");
+            
+
+            EmployeeNameList.Add(employeeName);
+           
+            //Console.WriteLine("name: " + employeeName + "     salary: " + employeeSalary + "kr");
 
         }
+
+        public void EmployeeSalary()
+        {
+            Console.WriteLine("Enter employee salary please:");
+            float employeeSalary = int.Parse(Console.ReadLine());
+
+           
+            EmployeeSarayList.Add(employeeSalary);
+
+
+        }
+        public void EmployeeInfo()
+        {
+
+        }
+
+
+
     }
 }
--- Ovning1/Program.cs
+++ Ovning1/Program.cs
@@ -7,8 +7,19 @@ namespace Ovning1
         static void Main(string[] args)
         {
             Employee call = new Employee();
-            call.EmployeeNameSalary();
-            
+
+            Console.WriteLine("hello  Do you want to add an employee  write yes or no ");
+            string answer = Console.ReadLine();
+            if(answer == "yes")
+            {
+                call.EmployeeName();
+                call.EmployeeSalary();
+
+
+            }
+
+
+
         }
     }
 }
